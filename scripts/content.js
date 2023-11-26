094c0e738ae552cfba3a055cf939564db8672ab8
Ke Ning <mintnick@gmail.com>
2023-11-26 12:34:56 +0800

    render button, get url

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,20 @@
-console.log('cosmos!')
+let url = "";
+
+window.addEventListener("load", renderBtn, false);
+
+function renderBtn(event) {
+  const main = document.getElementsByTagName('main')[0];
+
+  const btn = document.createElement('button');
+  btn.textContent = "Download";
+
+  if (main) {
+    main.prepend(btn);
+  }
+
+  // get url
+  const audio = document.getElementsByTagName('audio')[0];
+  if (audio) {
+    url = audio.getAttribute('src')
+  }
+}
